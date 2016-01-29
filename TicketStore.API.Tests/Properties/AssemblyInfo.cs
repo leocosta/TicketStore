27e000663f29ba482c693c4f9bbcc24f7bf2c4eb
Leonardo Costa <leotasco@gmail.com>
2016-01-29 11:23:17 -0200

    fixing an error occurred while compiling

--- TicketStore.API.Tests/Properties/AssemblyInfo.cs
+++ TicketStore.API.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TicketStore.API")]
+[assembly: AssemblyTitle("TicketStore.API.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TicketStore.API")]
+[assembly: AssemblyProduct("TicketStore.API.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("90821449-72bb-4e4f-84ca-79476841fdaf")]
+[assembly: Guid("8760d34a-a870-4b44-a796-e8863af23e2f")]
 
 // Version information for an assembly consists of the following four values:
 //
